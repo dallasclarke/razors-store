7ae76b57c1f803a505a6a1f62f3f082819fbf464
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-10-07 23:53:51 -0700

    Starting on homepage and did a basic setup for routing.

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script>
-	import Login from "./pages/Login.svelte"
-	import Checkout from "./pages/Checkout.svelte";
-	import ProductTemp from "./pages/ProductTemp.svelte";
-	import Products from "./pages/Products.svelte";
-	import Home from "./pages/Home.svelte";
-	import About from "./pages/About.svelte";
+  import Login from "./pages/Login.svelte";
+  import Checkout from "./pages/Checkout.svelte";
+  import ProductTemp from "./pages/ProductTemp.svelte";
+  import Products from "./pages/Products.svelte";
+  import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
+
+  import { Router, Route, Link } from "svelte-routing";
 </script>
 
+<Router>
+  <nav class="navbar">
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/products">Products</Link>
+  </nav>
+  <Route path="/" component={Home} />
+  <Route path="/about" component={About} />
+  <Route path="/login" component={Login} />
+  <Route path="/products" component={Products} />
+  <Route path="/products/:id" component={ProductTemp} />
+</Router>
---
+++ src/pages/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { link } from "svelte-routing";
+
+  import Hero from "../components/Hero.svelte";
+</script>
+
+<Hero>
+  <a href="/products" class="btn btn-primary btn-hero" use:link>Shop Now</a>
+</Hero>
