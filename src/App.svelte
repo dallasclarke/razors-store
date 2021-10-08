8e4505a89f1484078fbaf9c1ff7b8ceefce07c4d
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-10-07 23:09:18 -0700

    Initial

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Login from "./pages/Login.svelte"
+	import Checkout from "./pages/Checkout.svelte";
+	import ProductTemp from "./pages/ProductTemp.svelte";
+	import Products from "./pages/Products.svelte";
+	import Home from "./pages/Home.svelte";
+	import About from "./pages/About.svelte";
+</script>
+
