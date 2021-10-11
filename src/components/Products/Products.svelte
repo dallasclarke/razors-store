1e1e6ed358e0c3dd198ccd9827481dc3b19974e2
Dallas <dallas.ag.clarke@gmail.com>
2021-10-10 23:39:55 -0700

    Working on displaying products and setting up svelte store.

---
+++ src/components/Products/Products.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { onMount, onDestroy } from "svelte";
+
+  import products from "../../stores/defaultProducts";
+
+  
+</script>
+
+{#each $products as product}
+  <h2>{product.title}</h2>
+{/each}
---
+++ src/pages/Products.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Products from "../components/Products/Products.svelte";
+</script>
+
+<h1>
+    Hello from products page
+</h1>
+<Products />
