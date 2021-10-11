3efbe8dd2bea558981192a35e85d983cb814a225
Dallas <dallas.ag.clarke@gmail.com>
2021-10-10 23:59:23 -0700

    Created Product component for each product using props. Need to fix a bug that I am looking for that is causing nothing to be displayed.

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -2,10 +2,16 @@
   import { onMount, onDestroy } from "svelte";
 
   import products from "../../stores/defaultProducts";
+  import Product from "./Product.svelte";
 
-  
+  export let title = "";
 </script>
 
-{#each $products as product}
-  <h2>{product.title}</h2>
-{/each}
+<section class="section">
+  <h2 class="section-title">{title}</h2>
+  <div class="products-center">
+    {#each $products as product (product.id)}
+      <Product {product} />
+    {/each}
+  </div>
+</section>
--- src/pages/Products.svelte
+++ src/pages/Products.svelte
@@ -2,7 +2,4 @@
     import Products from "../components/Products/Products.svelte";
 </script>
 
-<h1>
-    Hello from products page
-</h1>
-<Products />
+<Products title="Our Products" />
